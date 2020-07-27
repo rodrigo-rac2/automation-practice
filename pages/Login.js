6c4a9b874c20f220e8c1c4375f05bf025c9767e5
Rodrigo Costa <60664391+rodrigo-rac2@users.noreply.github.com>
2020-07-27 11:30:32 -0300

    .

--- pages/Login.js
+++ pages/Login.js
@@ -1,22 +1,30 @@
 var actions = {
-    setEmail: function(email) {
-        return this.setValue('@email', email)
-    }, 
-    setPassword: function(password) {
-        return this.setValue('@password', password)
+    setEmail: function (email) {
+        return this
+            .waitForElementVisible('@email', 5000)
+            .clearValue('@email')
+            .setValue('@email', email)
     },
-    clickSignInButton: function() {
-        return this.click('@signInButton')
-    } 
+    setPassword: function (password) {
+        return this
+            .waitForElementVisible('@password', 5000)
+            .clearValue('@password')
+            .setValue('@password', password)
+    },
+    clickSignInButton: function () {
+        return this
+            .waitForElementVisible('@signInButton', 5000)
+            .click('@signInButton')
+    }
 }
 var checks = {
-    assertPageIsLoaded: function() {
-        return this.waitForElementVisible('#email', 5000)
+    assertPageIsLoaded: function () {
+        return this.waitForElementVisible('@email', 5000) //rewrite
     }
 }
 
 module.exports = {
-    commands: [ actions, checks ],
+    commands: [actions, checks],
     elements: {
         email: '#email',
         password: '#passwd',
